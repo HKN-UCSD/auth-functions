90f6a1390400999bf500c5bede5a4d788b9f4b4d
gypang <gypang24@gmail.com>
2020-03-03 19:55:21 -0800

    Debugged version of function.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,11 +10,11 @@ const admin = require('firebase-admin');
 // });
 
 /* data = {email: string (person to change role of),
-            role: string (role to change to)} */
-exports.addClaim = functions.https.onCall((data) => {
-    
+            role: string (role to change to)} */ 
+exports.addClaim = functions.https.onCall(async (data) => {
+
     // check caller exists
-    /*if (!context.auth) {
+    if (!context.auth) {
         throw new functions.https.HttpsError(
             "failed-precondition",
             "The function must be called while authenticated."
@@ -30,56 +31,52 @@ exports.addClaim = functions.https.onCall((data) => {
             "The function must be called by user with an officer token."
          );
     }
-    
+
     // get officer id from db
     const officer_id = getIdFromRoles("Officer");
-                        
+
     // check if caller doc has role of officer
-    admin.firestore().collection('users').doc(caller_uid).get().then(docSnapshot => {
-        if (docSnapshot.get('role_id') != officer_id) {
-            throw new functions.https.HttpsError(
-                "failed-precondition",
-                "The function must be called by user with an officer role."
-             );
-        }
-    }).catch(err => {
-        throw new functions.https.HttpsError(error.code, error.details);
-    });*/
-    
+    const user_doc = await admin.firestore().collection('users').doc(caller_uid).get()
+    if (user_doc.get('role_id') !== officer_id) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called by user with an officer role."
+        )
+    }
+
     // verified caller, find user
     const email = data.email;
     const role = data.role;
 
     // check if user exists in auth
-    const user_uid = admin.auth().getUserByEmail(email).then(UserRecord => {
-        return UserRecord.uid;
-    }).catch(err => {
-        throw new functions.https.HttpsError(error.code, error.details);
-    });
+    const user = await admin.auth().getUserByEmail(email);
+    const user_uid = user.uid;
 
     // get role_id from db
-    const roleID = getIdFromRoles(role);
+    const roleID = await getIdFromRoles(role);
 
     // check for user doc and set role
-    admin.firestore().collection('users').doc(user_uid).update({role_id: roleID}).catch(err => {
-        throw new functions.https.HttpsError(error.code, error.details);
+    await admin.firestore().collection('users').doc(user_uid).update({role_id: roleID}).catch(err => {
+        throw new functions.https.HttpsError(err.code, err.details);
     });
 
     // set claim for role
-    admin.auth().setCustomUserClaims(user.uid, {[role]: true});
+    await admin.auth().setCustomUserClaims(user.uid, {[role]: true}).catch(err => {
+        throw new functions.https.HttpsError(err.code, err.details);
+    });
 
     return {success: true}
 });
 
 function getIdFromRoles(role) {
     return admin.firestore().collection('roles').where("value","==",role).get()
-    .then(docSnapshot => {
-         if(docSnapshot.docs.empty) {
-            throw new functions.https.HttpsError(
-                "not-found",
-                "Role " + role + " does not exist."
-            )
-         }
-        return docSnapshot.docs[0].id
-    })
+        .then(querySnapshot => {
+            if(querySnapshot.empty) {
+                throw new functions.https.HttpsError(
+                    "not-found",
+                    "Role " + role + " does not exist."
+                )
+            }
+            return querySnapshot.docs[0].id
+        })
 }
